008d86f256e9102e78dd4a7194a8ac7ec2d5a7a9
ajay-kiran-reddy <ajaykiranreddy999@gmail.com>
2024-07-15 19:53:53 +0530

    delete and edit file features

--- src/components/fileExplorer/FileExplorer.tsx
+++ src/components/fileExplorer/FileExplorer.tsx
@@ -2,28 +2,35 @@ import masterData from "./mockData.json";
 import "./fileExplorer.css";
 import { useState } from "react";
 import { FiFolderPlus } from "react-icons/fi";
-
 import { LuFilePlus } from "react-icons/lu";
 import useFileActions from "./useFileActions";
 import { CiFileOn } from "react-icons/ci";
+import { LuDelete } from "react-icons/lu";
+import { MdEdit } from "react-icons/md";
 
 const FileExplorerTree = (props: any) => {
-  const { data, onAddDataCb } = props;
+  const { data, onAddDataCb, onDeleteDataCb, onEditDataCb } = props;
   const [hide, setHide] = useState(false);
   const [showInput, setShowInput] = useState(false);
   const [inputValue, setInputValue] = useState("");
-  const { insertNode } = useFileActions();
+  const { insertNode, deleteNode, editNode } = useFileActions();
   const [selectedNode, setSelectedNode]: any = useState({
     isFolder: false,
     folderId: null,
   });
+  const [isEdititng, setIsEditing] = useState(false);
+  const [actions, setActions] = useState({
+    open: false,
+    activeId: null,
+  });
 
-  const handleHideFolder = (e: any) => {
+  const handleHideFolder = () => {
     setHide(!hide);
   };
 
   const handleNewFolderClick = (e: any, node: any) => {
     e.stopPropagation();
+    setIsEditing(false);
     setShowInput(true);
     setHide(false);
     setSelectedNode({
@@ -35,6 +42,7 @@ const FileExplorerTree = (props: any) => {
   const handleNewFileClick = (e: any, node: any) => {
     e.stopPropagation();
     setShowInput(true);
+    setIsEditing(false);
     setHide(false);
     setSelectedNode({
       folderId: node.id,
@@ -49,14 +57,17 @@ const FileExplorerTree = (props: any) => {
       folderId: selectedNode.folderId,
     };
 
-    console.log(data, "[DATA]");
-
     if (event.keyCode === 13) {
-      onAddDataCb(data);
-      setSelectedNode({
-        folderId: null,
-        isFolder: false,
-      });
+      if (isEdititng) {
+        console.log(isEdititng, "[is eiting]");
+        onEditDataCb({ name: inputValue, itemId: selectedNode.folderId });
+      } else {
+        onAddDataCb(data);
+        setSelectedNode({
+          folderId: null,
+          isFolder: false,
+        });
+      }
       setShowInput(false);
       setInputValue("");
     }
@@ -73,13 +84,82 @@ const FileExplorerTree = (props: any) => {
     setShowInput(false);
   };
 
+  const handleDeleteData = (cbData: any) => {
+    const { isFolder, folderId } = cbData;
+    const resultTree = deleteNode(data, folderId, isFolder);
+    return resultTree;
+  };
+
+  const handleDeleteClick = (
+    event: any,
+    folderId: number,
+    isFolder: boolean
+  ) => {
+    event.stopPropagation();
+    onDeleteDataCb({
+      folderId,
+      isFolder,
+    });
+  };
+
+  const handleEditClick = (event: any, folderData: any) => {
+    console.log("clicking edit icon", folderData);
+    event.stopPropagation();
+    setIsEditing(true);
+    setShowInput(true);
+    setInputValue(folderData.name);
+    setSelectedNode({
+      folderId: folderData.id,
+      isFolder: true,
+    });
+  };
+
+  const handleEditData = (cbData: any) => {
+    const { name, itemId } = cbData;
+    const resultTree = editNode(data, itemId, name);
+    setIsEditing(false);
+    return resultTree;
+  };
+
   return (
     <>
       <div className="fileContainer">
+        {showInput && (
+          <div className="newFolder">
+            <input
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              onKeyDown={(e) => onAddNode(e)}
+              autoFocus
+              onBlur={handleOnBlur}
+            />
+          </div>
+        )}
+
         {data.isFolder ? (
           <>
-            <div className={"folder"} onClick={handleHideFolder}>
+            <div
+              className={"folder"}
+              onClick={handleHideFolder}
+              onMouseEnter={() => setActions({ open: true, activeId: data.id })}
+              onMouseLeave={() => setActions({ open: false, activeId: null })}
+            >
               📁 {data.name}
+              {actions.open && (
+                <div id="actions">
+                  <LuDelete
+                    onClick={(e) =>
+                      handleDeleteClick(e, data.id, data.isFolder)
+                    }
+                  />
+                  <span
+                    style={{ paddingTop: "5px" }}
+                    onClick={(e) => handleEditClick(e, data)}
+                  >
+                    <MdEdit />
+                  </span>
+                </div>
+              )}
               <span
                 style={{ paddingTop: "5px" }}
                 onClick={(e) => handleNewFolderClick(e, data)}
@@ -94,29 +174,45 @@ const FileExplorerTree = (props: any) => {
               </span>
             </div>
 
-            {showInput && (
-              <div className="newFolder">
-                <input
-                  value={inputValue}
-                  onChange={(e) => setInputValue(e.target.value)}
-                  onKeyDown={(e) => onAddNode(e)}
-                  autoFocus
-                  onBlur={handleOnBlur}
-                />
-              </div>
-            )}
-
             <div
               style={{ display: hide ? "none" : "block", marginLeft: "15px" }}
             >
               {data?.children.map((d: any) => {
-                return <FileExplorerTree data={d} onAddDataCb={handleData} />;
+                return (
+                  <FileExplorerTree
+                    data={d}
+                    onAddDataCb={handleData}
+                    onDeleteDataCb={handleDeleteData}
+                    onEditDataCb={handleEditData}
+                  />
+                );
               })}
             </div>
           </>
         ) : (
-          <div className="fileItem">
+          <div
+            className="fileItem"
+            onMouseEnter={() => setActions({ open: true, activeId: data.id })}
+            onMouseLeave={() => setActions({ open: false, activeId: null })}
+          >
             <CiFileOn /> {data.name}
+            {actions.open && (
+              <div id="actions">
+                <span style={{ marginRight: "5px" }}>
+                  <LuDelete
+                    onClick={(e) =>
+                      handleDeleteClick(e, data.id, data.isFolder)
+                    }
+                  />
+                </span>
+                <span
+                  style={{ paddingTop: "5px" }}
+                  onClick={(e) => handleEditClick(e, data)}
+                >
+                  <MdEdit />
+                </span>
+              </div>
+            )}
           </div>
         )}
       </div>
@@ -125,19 +221,35 @@ const FileExplorerTree = (props: any) => {
 };
 
 const FileExplorer = () => {
-  const [data, setData] = useState(masterData);
-  const { insertNode } = useFileActions();
+  const [data, setData]: any = useState(masterData);
+  const { insertNode, deleteNode, editNode } = useFileActions();
 
   const handleData = (cbData: any) => {
     const { name, isFolder, folderId } = cbData;
     const resultTree = insertNode(data, name, folderId, isFolder);
-    console.log(resultTree, "[resultTree]");
     setData(resultTree);
   };
 
-  console.log(data, "[TREE DATA]");
+  const handleDeleteData = (cbData: any) => {
+    const { isFolder, folderId } = cbData;
+    const results = deleteNode(data, folderId, isFolder);
+    setData(results);
+  };
 
-  return <FileExplorerTree data={data} onAddDataCb={handleData} />;
+  const handleEditData = (cbData: any) => {
+    const { name, itemId } = cbData;
+    const resultTree = editNode(data, itemId, name);
+    setData(resultTree);
+  };
+
+  return (
+    <FileExplorerTree
+      data={data}
+      onAddDataCb={handleData}
+      onDeleteDataCb={handleDeleteData}
+      onEditDataCb={handleEditData}
+    />
+  );
 };
 
 export { FileExplorerTree };
--- src/components/fileExplorer/useFileActions.ts
+++ src/components/fileExplorer/useFileActions.ts
@@ -23,7 +23,44 @@ const useFileActions = () => {
     }
   };
 
-  return { insertNode };
+  const deleteNode = (tree: any, folderId: number, isFolder: boolean) => {
+    console.log(isFolder);
+    if (tree.id === folderId) {
+      console.log("record matched");
+      if (isFolder) {
+        tree.children = [];
+      } else {
+        // console.log("inside else");
+        // delete tree.name;
+        // delete tree.id;
+        // delete tree.children;
+        // delete tree.isFolder;
+        console.log(tree, "[TREE inside else]");
+      }
+      return tree;
+    } else {
+      let filterdData = [];
+      filterdData = tree?.children?.map((child: any) => {
+        return deleteNode(child, folderId, isFolder);
+      });
+      return { ...tree, children: filterdData };
+    }
+  };
+
+  const editNode = (tree: any, itemId: number, name: string) => {
+    if (tree.id === itemId) {
+      tree.name = name;
+      return tree;
+    } else {
+      let finalData = [];
+      finalData = tree?.children?.map((child: any) => {
+        return editNode(child, itemId, name);
+      });
+      return { ...tree, children: finalData };
+    }
+  };
+
+  return { insertNode, deleteNode, editNode };
 };
 
 export default useFileActions;
