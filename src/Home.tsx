0470ea32754ce7fc5bd4fb2f3be852ce4721317e
ajay-kiran-reddy <ajaykiranreddy999@gmail.com>
2024-07-26 09:34:16 +0530

    ant design integrated

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,101 +1,88 @@
-import Card from "./common/Card";
 import { useNavigate } from "react-router-dom";
 import "./App.css";
+
+import React from "react";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Avatar, Card, Col, Row, Tooltip } from "antd";
+import { IoRocketOutline } from "react-icons/io5";
+
+const { Meta } = Card;
+
+const CardsContent = [
+  {
+    title: "Toast Message",
+    description:
+      "This card contains functionality related to toast bar meesage",
+    path: "/toast",
+  },
+  {
+    title: "Pagination",
+    description: "This card contains functionality related to pagination",
+    path: "/pagination",
+  },
+  {
+    title: "Progress Bar",
+    description: "This card contains functionality related to Progress bar",
+    path: "/progressBar",
+  },
+  {
+    title: "Auto Complete",
+    description:
+      "This card contains functionality related to Auto Complete bar",
+    path: "/autoComplete",
+  },
+  {
+    title: "Stopwatch",
+    description: "This card contains functionality related to stopwatch",
+    path: "/stopwatch",
+  },
+  {
+    title: "File Explorer",
+    description: "This card contains functionality related to File explorer",
+    path: "/fileExplorer",
+  },
+  {
+    title: "Nested Comments",
+    description: "This card contains functionality related to Nested comments",
+    path: "/comments",
+  },
+];
 
 const Home = () => {
   const navigate = useNavigate();
 
   return (
     <div>
-      <h3>Welcome to Ajay Portfolio</h3>
-      <div className="gridContainer">
-        <div>
-          <Card
-            title="Toast Message"
-            body={
-              <>This card contains functionality related to toast bar meesage</>
-            }
-            actions={
-              <>
-                <button onClick={() => navigate("/toast")}>Open</button>
-              </>
-            }
-          />
-        </div>
-        <div>
-          <Card
-            title="Pagination"
-            body={<>This card contains functionality related to pagination</>}
-            actions={
-              <>
-                <button onClick={() => navigate("/pagination")}>Open</button>
-              </>
-            }
-          />
-        </div>
-        <div>
-          <Card
-            title="Progress Bar"
-            body={<>This card contains functionality related to Progress bar</>}
-            actions={
-              <>
-                <button onClick={() => navigate("/progressBar")}>Open</button>
-              </>
-            }
-          />
-        </div>
-        <div>
-          <Card
-            title="Auto Complete"
-            body={
-              <>This card contains functionality related to Auto Complete bar</>
-            }
-            actions={
-              <>
-                <button onClick={() => navigate("/autoComplete")}>Open</button>
-              </>
-            }
-          />
-        </div>
-        <div>
-          <Card
-            title="Stopwatch"
-            body={<>This card contains functionality related to stopwatch</>}
-            actions={
-              <>
-                <button onClick={() => navigate("/stopwatch")}>Open</button>
-              </>
-            }
-          />
-        </div>
-        <div>
-          <Card
-            title="File Explorer"
-            body={
-              <>This card contains functionality related to File explorer</>
-            }
-            actions={
-              <>
-                <button onClick={() => navigate("/fileExplorer")}>Open</button>
-              </>
-            }
-          />
-        </div>
-
-        <div>
-          <Card
-            title="Nested Comments"
-            body={
-              <>This card contains functionality related to Nested comments</>
-            }
-            actions={
-              <>
-                <button onClick={() => navigate("/comments")}>Open</button>
-              </>
-            }
-          />
-        </div>
-      </div>
+      <Row gutter={[48, 16]}>
+        {CardsContent.map((card) => {
+          return (
+            <Col lg={6} md={8} sm={12} xs={24} span={5}>
+              <Card
+                cover={
+                  <img
+                    alt="example"
+                    src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                  />
+                }
+                actions={[
+                  <Tooltip title="Launch">
+                    <IoRocketOutline
+                      size={20}
+                      onClick={() => navigate(card.path)}
+                    />
+                  </Tooltip>,
+                ]}
+              >
+                <Meta title={card.title} description={card.description} />
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
     </div>
   );
 };
